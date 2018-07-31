8b801c202c395b231e4157baee67b011505d5ede
Pedro Resende <pedro.resende@kwanko.com>
2018-07-31 15:34:55 +0100

    Refactor done by Cédric Chapard
    Principaux changements :
    - Changement de ce qui est injecté dans l'objet RpcError;
    - Ajout du code d'erreur;
    - Ajout de méthodes `hasXXX()`;
    - Ré-indentation (le jslinter n'aimait pas les 4 espaces);

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
 'use strict';
 
 var defaultHeaders = {
-    Accept: 'application/json, text/plain, */*',
-    'Content-Type': 'application/json',
-    'X-Requested-With': 'XMLHttpRequest'
+  Accept: 'application/json, text/plain, */*',
+  'Content-Type': 'application/json',
+  'X-Requested-With': 'XMLHttpRequest'
 };
 
 var settings = {
   lastId: 1,
   endPoint: null,
   headers: defaultHeaders,
-  credentials: 'include',
-  debug: false,
-}
+  credentials: null,
+  debug: null
+};
 
-function JsonRpcClient(endPoint, debug) {
+function JsonRpcClient(endPoint, credentials = 'include', debug = false) {
   settings.endPoint = endPoint;
-  if (debug !== '') {
-    settings.debug = debug;
-  }
+  settings.credentials = credentials;
+  settings.debug = debug;
 }
 
-JsonRpcClient.prototype.request = function(method, params) {
-  let id = settings.lastId;
-  id++;
-  settings.lastId = id;
-
-  if (params === '') {
-    params = {};
-  }
+JsonRpcClient.prototype.request = function(method, params = {}) {
+  let id = settings.lastId++;
 
   let req = {
     method: 'POST',
@@ -38,87 +31,95 @@ JsonRpcClient.prototype.request = function(method, params) {
       jsonrpc: '2.0',
       id: id,
       method: method,
-      params: params,
-    }),
+      params: params
+    })
   };
 
   return fetch(settings.endPoint, req)
-      .then(res => checkStatus(res))
-      .then(res => parseJSON(res))
-      .then(res => checkError(res, req, settings.debug))
-      .then(res => logResponse(res, settings.debug));
+    .then(res => checkStatus(res))
+    .then(res => parseJSON(res))
+    .then(res => checkError(res, req, settings.debug))
+    .then(res => logResponse(res, settings.debug));
 }
 
 function parseJSON(response) {
-    return response.json();
+  return response.json();
 }
 
 function checkStatus(response) {
-    // we assume 400 as valid code here because it's the default return code when sth has gone wrong,
-    // but then we have an error within the response, no?
-    if (response.status >= 200 && response.status <= 400) {
-        return response;
-    }
+  // we assume 400 as valid code here because it's the default return code when sth has gone wrong,
+  // but then we have an error within the response, no?
+  if (response.status >= 200 && response.status <= 400) {
+    return response;
+  }
 
-    const error = new Error(response.statusText);
-    error.response = response;
-    throw error;
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
 }
 
 function checkError(data, req, debug = false) {
-    if (data.error) {
-        /* eslint-disable no-console */
-        if (debug === true && console && console.error) {
-            console.error(`Request ID ${data.id} failed: ${data.error}`);
-        } else if (debug === true && console && console.log) {
-            console.log(`Request ID ${data.id} failed: ${data.error}`);
-        }
-        /* eslint-enable no-console */
-
-        const error = new RpcError(data.error, req, data);
-        error.response = data;
-
-        throw error;
+  if (data.error) {
+    /* eslint-disable no-console */
+    if (debug === true && console && console.error) {
+      console.error(`Request ID ${data.id} failed: ${data.error}`);
+    } else if (debug === true && console && console.log) {
+      console.log(`Request ID ${data.id} failed: ${data.error}`);
     }
+    /* eslint-enable no-console */
 
-    return data;
+    throw new RpcError(data.error.message, req, data.error.data, data.error.code);
+  }
+
+  return data;
 }
 
 function logResponse(response, debug = false) {
-    if (debug === true) {
-        /* eslint-disable no-console */
-        console.log('Got response for id', response.id, 'with response', response.result);
-        console.log('Response message for request', response.id, ':', response.result.message);
-        /* eslint-enable no-console */
-    }
-
-    return response.result;
+  if (debug === true) {
+    /* eslint-disable no-console */
+    console.log('Got response for id', response.id, 'with response', response.result);
+    console.log('Response message for request', response.id, ':', response.result.message);
+    /* eslint-enable no-console */
+  }
+  return response.result;
 }
 
 /**
  * RpcError is a simple error wrapper holding the request and the response.
  */
 export class RpcError extends Error {
-    constructor(message, request, response) {
-        super(message);
+  constructor(message, request, response, code) {
+    super(message);
+    this.name = 'RpcError';
+    this.message = message || '';
+    this.request = request;
+    this.response = response;
+    this.code = code;
+  }
 
-        this.name = 'RpcError';
-        this.message = (message || '');
-        this.request = request;
-        this.response = response;
-    }
+  toString() {
+    return this.message;
+  }
 
-    toString() {
-        return this.message;
-    }
+  getRequest() {
+    return this.request;
+  }
 
-    getRequest() {
-        return this.request;
-    }
+  getResponse() {
+    return this.response;
+  }
 
-    getResponse() {
-        return this.response;
-    }
+  getCode() {
+    return this.code;
+  }
+
+  hasMessage() {
+    return this.message !== undefined;
+  }
+
+  hasResponse() {
+    return this.response !== undefined;
+  }
 }
 
 export default JsonRpcClient;
